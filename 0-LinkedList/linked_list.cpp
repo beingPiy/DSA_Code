b6c53934348abaf2cc98c87821ce8ab3abd896fa
Piyush Pandey <piyushpandey379@gmail.com>
2021-06-04 15:54:16 +0530

    initial commit

---
+++ 0-LinkedList/linked_list.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std ;
+
+class node{
+    private :
+        int data ;
+        node * next ;
+
+    public :
+        node(int d){
+            data = d ;
+            next = NULL ;
+        }
+
+} ;
+
+void insertAtHead(node* & head , int d){
+     if(head == NULL){
+         head = new node(d) ;
+         return ;
+
+     }
+
+     node* n = new node(d) ;
+     n -> next = head ;
+     head = n ;
+}
+
+void print(node * head){
+    while(head != NULL){
+        cout << head -> data << " -> " ;
+        head = head -> next ;
+    }
+    cout << endl ;
+}
+
+int main(){
+
+    node * head = NULL ;
+    insertAtHead(head , 3) ;
+    insertAtHead(head , 2) ;
+    insertAtHead(head , 1) ;
+    insertAtHead(head , 0) ;
+
+    return 0 ;
+}
